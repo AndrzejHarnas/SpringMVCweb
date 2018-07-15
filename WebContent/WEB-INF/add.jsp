0218ed1d386601dcf0adc25f8348df7411208f35
Harnas <harnas@o2.pl>
2018-07-15 14:55:49 +0200

    2 pages

---
+++ WebContent/WEB-INF/add.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1" %>
+<html>
+<head>
+</head>
+<body>
+<h1> Wynik dodawania to: ${sum} </h1>
+
+</body>
+
+</html>
--- src/main/java/solution/HomeController.java
+++ src/main/java/solution/HomeController.java
@@ -1,8 +1,12 @@
 package solution;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
@@ -12,6 +16,18 @@ public class HomeController {
 
         model.addAttribute("nick","Andrzej");
         return "home";
+    }
+    @RequestMapping(value ="/add")
+    public String add(HttpServletRequest request, HttpServletResponse response, Model addModell)
+    {
+        int i = Integer.parseInt(request.getParameter("t1"));
+        int j = Integer.parseInt(request.getParameter("t2"));
+
+        int k = i+j;
+
+       addModell.addAttribute("sum",k);
+
+     return "add";
     }
 
 }
